b9e0576310658b174fd3af2420f19f1b2406cbd6
Guido Steinacker <guido.steinacker@otto.de>
2017-11-26 19:37:37 +0100

    Styling of external Links Opening external links in tab/window Proper disabling of unavailable paging buttons

--- hal-odyssey-core/src/main/java/de/otto/edison/hal/browser/controller/PagerModel.java
+++ hal-odyssey-core/src/main/java/de/otto/edison/hal/browser/controller/PagerModel.java
@@ -12,7 +12,7 @@ import static java.util.Optional.empty;
 
 public class PagerModel {
     public static final Set<String> PAGING_RELS = new HashSet<>(asList("first", "next", "prev", "previous", "last"));
-    public static final PagerModel UNAVAILABLE = new PagerModel(null, null, null, null);
+    public static final PagerModel UNAVAILABLE = new PagerModel("", null, null, null, null);
 
     public final boolean available;
     public final LinkModel first;
@@ -20,20 +20,30 @@ public class PagerModel {
     public final LinkModel prev;
     public final LinkModel next;
 
-    private PagerModel(final LinkModel first,
+    private PagerModel(final String self,
+                       final LinkModel first,
                        final LinkModel prev,
                        final LinkModel next,
                        final LinkModel last) {
-        this.first = first;
-        this.last = last;
-        this.prev = prev;
-        this.next = next;
+        this.first = nonSelf(first, self);
+        this.prev = nonSelf(prev, self);
+        this.next = nonSelf(next, self);
+        this.last = nonSelf(last, self);
         this.available = first != null || last != null || prev != null || next != null;
+    }
+
+    private LinkModel nonSelf(final LinkModel linkModel, final String self) {
+        if (linkModel == null || linkModel.href.equals(self)) {
+            return null;
+        } else {
+            return linkModel;
+        }
     }
 
     public static PagerModel toPagerModel(final HalRepresentation hal) {
         final Links links = hal.getLinks();
         return new PagerModel(
+                links.getLinkBy("self").map(Link::getHref).orElse(""),
                 toLinkModel(links.getLinkBy("first").orElse(null)),
                 toLinkModel(links.getLinkBy("prev").orElse(null), links.getLinkBy("previous").orElse(null)),
                 toLinkModel(links.getLinkBy("next").orElse(null)),
