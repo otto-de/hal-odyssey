36526587940653c444864a2ca0db3161b6a7ef9e
Guido Steinacker <guido.steinacker@otto.de>
2017-12-01 14:17:21 +0100

    Added example json documents + removed special treatment of example in HttpClient

--- hal-odyssey-core/src/main/java/de/otto/edison/hal/odyssey/service/HttpClient.java
+++ hal-odyssey-core/src/main/java/de/otto/edison/hal/odyssey/service/HttpClient.java
@@ -6,84 +6,13 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.valueOf;
 
 @Service
 public class HttpClient {
-
-    private static final String EXAMPLE_JSON = "{" +
-            "   \"foo\":\"bar\"," +
-            "   \"foobar\":{\"one\":\"eins\",\"two\":\"zwei\"}," +
-            "   \"barfoo\":[{\"one\":\"eins\"},{\"two\":\"zwei\"}]," +
-            "   \"_links\":{" +
-            "       \"curies\": [" +
-            "           {\"name\":\"example\", \"href\":\"http://example.com/rels/{rel}\", \"templated\": true}," +
-            "           {\"name\":\"otto\", \"href\":\"http://spec.otto.de/rels/{rel}\", \"templated\": true}" +
-            "       ]," +
-            "       \"self\": {" +
-            "           \"href\":\"%s\", \"title\":\"Some Resource\", \"type\":\"application/hal+json\", \"profile\":\"http://example.com/profiles/example\"" +
-            "       }," +
-            "       \"first\": {" +
-            "           \"href\":\"http://localhost:8080/example?page=0\"" +
-            "       }," +
-            "       \"next\": {" +
-            "           \"href\":\"http://localhost:8080/example?page=5\"" +
-            "       }," +
-            "       \"prev\": {" +
-            "           \"href\":\"http://localhost:8080/example?page=3\"" +
-            "       }," +
-            "       \"last\": {" +
-            "           \"href\":\"http://localhost:8080/example?page=42\"" +
-            "       }," +
-            "       \"example:foo\": [" +
-            "           {\"href\":\"http://localhost:8080/example/foo/42\", \"title\":\"Foo 42\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}," +
-            "           {\"href\":\"http://localhost:8080/example/foo/43\", \"title\":\"Foo 43\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}," +
-            "           {\"href\":\"http://localhost:8080/example/foo/44\", \"title\":\"Foo 44\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}," +
-            "           {\"href\":\"http://localhost:8080/example/foo/45\", \"title\":\"Foo 45\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}," +
-            "           {\"href\":\"http://localhost:8080/example/foo/46\", \"title\":\"Foo 46\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}" +
-            "       ]," +
-            "       \"bar\": [" +
-            "           {\"href\":\"http://localhost:8080/example/bar/42\", \"title\":\"Bar 42\", \"type\":\"application/hal+json\"}," +
-            "           {\"href\":\"http://localhost:8080/example/bar/43\", \"title\":\"Bar 43\", \"type\":\"application/hal+json\"}," +
-            "           {\"href\":\"http://localhost:8080/example/bar{/id}{?x}{&y}\", \"templated\": true, \"title\":\"Bar 44\", \"type\":\"application/hal+json\"}," +
-            "           {\"href\":\"http://localhost:8080/example/bar/45\", \"title\":\"Bar 45\", \"type\":\"application/hal+json\"}," +
-            "           {\"href\":\"http://localhost:8080/example/bar/46\", \"title\":\"Bar 46\", \"type\":\"application/hal+json\"}" +
-            "       ]" +
-            "   }," +
-            "   \"_embedded\":{" +
-            "       \"example:foo\": [" +
-            "           {" +
-            "               \"_links\" : {" +
-            "                   \"self\" : {\"href\":\"http://localhost:8080/example/foo/42\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}" +
-            "               }," +
-            "               \"p1\":\"1\"," +
-            "               \"p2\":{\"one\":\"eins\",\"two\":\"zwei\"}," +
-            "               \"p3\":[{\"one\":\"eins\"},{\"two\":\"zwei\"}]" +
-            "           }," +
-            "           {" +
-            "               \"_links\" : {" +
-            "                   \"self\" : {\"href\":\"http://localhost:8080/example/foo/43\", \"title\":\"Foo 43\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}" +
-            "               }," +
-            "               \"p1\":\"2\"," +
-            "               \"p2\":{\"one\":\"eins\",\"two\":\"zwei\"}," +
-            "               \"p3\":[{\"one\":\"eins\"},{\"two\":\"zwei\"}]" +
-            "           }," +
-            "           {" +
-            "               \"_links\" : {" +
-            "                   \"self\" : {\"href\":\"http://localhost:8080/example/foo/44\", \"title\":\"Foo 44\", \"type\":\"application/hal+json\", \"profile\":\"http://localhost:8080/profiles/example;version=1\"}" +
-            "               }," +
-            "               \"p1\":\"3\"," +
-            "               \"p2\":{\"one\":\"eins\",\"two\":\"zwei\"}," +
-            "               \"p3\":[{\"one\":\"eins\"},{\"two\":\"zwei\"}]" +
-            "           }" +
-            "       ]" +
-            "   }" +
-            "}";
 
     private static final MediaType APPLICATION_HAL_JSON = valueOf("application/hal+json");
     private final RestTemplate restTemplate;
@@ -95,18 +24,11 @@ public class HttpClient {
 
     public ResponseEntity<String> get(final String href,
                                       final String type) {
-        if (href.startsWith("http://localhost:8080/example")) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.put(HttpHeaders.CONTENT_TYPE, asList("application/hal+json"));
-            headers.put(HttpHeaders.WARNING, asList("Example Content", "Served by Fake Service"));
-            return new ResponseEntity<>(format(EXAMPLE_JSON, href), headers, OK);
-        } else {
-            return restTemplate.exchange(
-                    href,
-                    HttpMethod.GET,
-                    new HttpEntity<String>(headersFor(type)),
-                    String.class);
-        }
+        return restTemplate.exchange(
+                href,
+                HttpMethod.GET,
+                new HttpEntity<String>(headersFor(type)),
+                String.class);
     }
 
     private HttpHeaders headersFor(final String type) {
