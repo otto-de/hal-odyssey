60794679da39964705b49eb7afc4d6513f9d92a5
Guido Steinacker <guido.steinacker@otto.de>
2017-12-02 20:02:30 +0100

    Added example server that is serving some example resources

---
+++ hal-odyssey-example-server/src/main/java/de/otto/edison/hal/odyssey/ExampleOdysseyServer.java
@@ -0,0 +1,12 @@
+package de.otto.edison.hal.odyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOdysseyServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleOdysseyServer.class, args);
+	}
+}
